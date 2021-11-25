3501b65c55749cffc023c3eab957e9f34a2699c6
xieguigang <xie.guigang@gcmodeller.org>
2021-11-25 11:21:06 +0800

    Create build.cmd

---
+++ .pkg/build.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+SET drive=%~d0
+SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net5.0
+SET pkg=./metagenomics.zip
+
+%R_HOME%/Rscript.exe --build ../ /save %pkg%
+%R_HOME%/R#.exe --install.packages %pkg%
+
+pause
