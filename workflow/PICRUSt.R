7054ea8c870e2c292fda529a4fbdd3a2570a562e
guigang xie <gg.xie@bionovogene.com>
2022-03-03 16:50:26 +0800

    Create PICRUSt.R

---
+++ workflow/PICRUSt.R
@@ -0,0 +1,21 @@
+require(GCModeller);
+
+imports "microbiome" from "metagenomics_kit";
+
+const otu_file as string = ?"--out_table" || stop("A mothur OTU table file must be provided!");
+const outfile as string  = ?"--out_KO"    || `${dirname(otu_file)}/metagenome.csv`;
+
+using PICRUSt as file("ko_13_5_precalculated.PICRUSt") {
+
+	OTUtable = read.csv(otu_file, row.names = "taxonomy");
+	OTUtable[, "OTU_num"] = NULL;
+	
+	metagenome = PICRUSt
+	|> read.PICRUSt_matrix()
+	|> predict_metagenomes(OTUtable)
+	|> as.data.frame()
+	;
+	
+	# run enrichment analysis
+	write.csv(metagenome, file = outfile, row.names = TRUE);
+}
