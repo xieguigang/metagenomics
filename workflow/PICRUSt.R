151504b25f5f2d7eb623d02920cce063622d85ce
guigang xie <gg.xie@bionovogene.com>
2022-03-03 17:18:27 +0800

    Update PICRUSt.R

--- workflow/PICRUSt.R
+++ workflow/PICRUSt.R
@@ -3,9 +3,10 @@ require(GCModeller);
 imports "microbiome" from "metagenomics_kit";
 
 const otu_file as string = ?"--out_table" || stop("A mothur OTU table file must be provided!");
-const outfile as string  = ?"--out_KO"    || `${dirname(otu_file)}/metagenome.csv`;
+const outfile  as string = ?"--out_KO"    || `${dirname(otu_file)}/metagenome.csv`;
+const matrixKO as string = ?"--matrix"    || "/opt/metagenomics/ko_13_5_precalculated.PICRUSt";
 
-using PICRUSt as file("ko_13_5_precalculated.PICRUSt") {
+using PICRUSt as file(matrixKO) {
 
 	OTUtable = read.csv(otu_file, row.names = "taxonomy");
 	OTUtable[, "OTU_num"] = NULL;
