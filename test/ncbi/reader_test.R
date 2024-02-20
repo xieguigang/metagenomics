94cf68c56d1b7ec78f612b069c23415cab3d13f7
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-02-20 10:48:21 +0800

    Update reader_test.R

--- test/ncbi/reader_test.R
+++ test/ncbi/reader_test.R
@@ -2,3 +2,16 @@ require(GCModeller);
 
 imports "taxonomy_kit" from "metagenomics_kit";
 
+let tree = Ncbi.taxonomy_tree("E:\\metagenomics\\data");
+let species = ranks(tree) |> taxonomy_ranks("species");
+let ncbi_taxid = [unlist(species)]::taxid;
+
+str(species);
+print(ncbi_taxid);
+
+for(node in tqdm(ncbi_taxid)) {
+    # str(node);
+    print(tree |> lineage(node));
+}
+
+pause();
