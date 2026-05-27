63133a47965e581e23981c5a6573c0774e7a0d12
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-27 18:06:00 +0800

    Update spearman_MIC.R

--- R/mixOmics/spearman_MIC.R
+++ R/mixOmics/spearman_MIC.R
@@ -1,3 +1,10 @@
-const spearman_MIC = function(x,y, output_dir) {
-    
+#' function for make association between the 16s microbiome OTU and metabolomics metabolites
+#' 
+#' @param x the OTU table
+#' @param y the metabolite expression data
+#' 
+#' @details x and y should be a dataframe in format of features in rows and samples in columns
+#' 
+const spearman_MIC = function(x,y, output_dir = "./") {
+
 }
