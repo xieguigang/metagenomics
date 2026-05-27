c068f86b7978da66d9cdeacb928eea1f56c4f0c6
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-05-27 20:42:14 +0800

    Create pfam.R

---
+++ test/pfam.R
@@ -0,0 +1,7 @@
+require(GCModeller);
+
+imports "bioseq.fasta" from "seqtoolkit";
+
+let seed = read_stockholm("C:\Users\Administrator\Downloads\Pfam-A.seed");
+
+write.fasta(seed, file = "Z:/Pfam-A.fas");
